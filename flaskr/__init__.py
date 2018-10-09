e3c187d59eec5bdde49d1288993a5c92ddbc989e
Rukeith <fenglan.crazydance@gmail.com>
2018-10-10 00:58:08 +0800

    Add db migrate
    Signed-off-by: Rukeith <fenglan.crazydance@gmail.com>

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -8,9 +9,10 @@ def create_app():
     app = Flask(__name__)
     app.config.update(
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_DATABASE_URI='mysql+pymysql://user:pass@127.0.0.1:3400/demo'
+        SQLALCHEMY_DATABASE_URI='mysql+pymysql://user:pass@127.0.0.1:3400/shopee24_dev'
     )
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     from flaskr.routes.demo import api_blueprint as demo_blueprint
     app.register_blueprint(demo_blueprint)
--- flaskr/routes/demo.py
+++ flaskr/routes/demo.py
@@ -1,10 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flaskr.models.demo import queryData
 
-api_blueprint = Blueprint('hello', __name__)
+api_blueprint = Blueprint('hello', __name__, url_prefix='/hello')
 
 
-@api_blueprint.route('/hello', methods=['GET'])
+@api_blueprint.route('/', methods=['GET'])
 def hello():
     queryData()
     return 'Hello, World!'
+
+
+@api_blueprint.route('/vv', methods=['GET'])
+def vv():
+    return render_template('index.html')
