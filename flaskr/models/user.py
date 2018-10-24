2b950ba83fb278106e7cd649afda6cd8de1d1ad7
Rukeith <fenglan.crazydance@gmail.com>
2018-10-24 14:45:49 +0800

    Update relationship
    Signed-off-by: Rukeith <fenglan.crazydance@gmail.com>

--- flaskr/models/user.py
+++ flaskr/models/user.py
@@ -4,18 +4,46 @@ from datetime import datetime
 from sqlalchemy.dialects.mysql import ENUM, JSON
 
 
+arrangment_user = db.Table(
+    'arrangment_user',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'), primary_key=True),
+    db.Column('arrangement_id', db.Integer, db.ForeignKey('arrangement.arrangement_id'), primary_key=True),
+    extend_existing=True
+)
+
+
 class User(db.Model):
     __tablename__ = 'user'
+    __table_args__ = {"useexisting": True}
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    search_condition = db.relationship('SearchCondition', uselist=False, backref='user')
+    email = db.Column(db.String(50), unique=True, nullable=False)
 
-    teachers = db.relationship('TeacherUser', backref=db.backref('users', lazy='joined'), lazy='dynamic')
-
-    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
-    classes = db.relationship('Classes', backref='classes')
-
-    books = db.relationship('Book',backref='book')
+    # one-to-one
+    search_condition = db.relationship(
+        'SearchCondition',
+        uselist=False,
+        backref='user'
+    )
+    # many-to-many
+    arrangements = db.relationship(
+        'Arrangement',
+        secondary=arrangment_user,
+        lazy='subquery',
+        backref=db.backref('participants', lazy='joined')
+    )
+    # one-to-many
+    self_expenses_record = db.relationship(
+        'ExpensesRecord',
+        lazy='subquery',
+        backref=db.backref('owner', lazy='joined')
+    )
+    # one-to-many
+    chatrooms = db.relationship(
+        'Chatroom',
+        lazy='subquery',
+        backref=db.backref('owner', lazy='joined')
+    )
 
     def __init__(self, username, email):
         self.username = username
@@ -31,29 +59,62 @@ class User(db.Model):
 
 class SearchCondition(db.Model):
     __tablename__ = 'search_condition'
+    __table_args__ = {"useexisting": True}
     search_condition_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-    condition = db.Column(JSON, server_default=text(r"{}"))
+    condition = db.Column(JSON, default=lambda: {})
+
+    def __init__(self, search_condition_id, user_id, condition):
+        self.search_condition_id = search_condition_id
+        self.user_id = user_id
+        self.condition = condition
+
+    def __repr__(self):
+        return '<SearchCondition %r>' % self.search_condition_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
 
-class TeacherUser(db.Model):
-    __tablename__ = 'teacher_user'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.teacher_id'))
+class Arrangement(db.Model):
+    __tablename__ = 'arrangement'
+    __table_args__ = {"useexisting": True}
+    arrangement_id = db.Column(db.Integer, primary_key=True)
 
 
-class Teacher(db.Model):
-    __tablename__ = 'teacher'
-    teacher_id = db.Column(db.Integer, primary_key=True)
-    participants = db.relationship('TeacherUser', backref=db.backref('teachers', lazy='joined'), lazy='dynamic')
+class ExpensesRecord(db.Model):
+    __tablename__ = 'expenses_record'
+    __table_args__ = {"useexisting": True}
+    record_id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(
+        ENUM('buy_candy', 'transfer_candy', 'spend_candy', 'exchange_money'),
+        nullable=False
+    )
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
 
-class Classes(db.Model):
-    __tablename__ = 'classes'
-    class_id = db.Column(db.Integer, primary_key=True)
+class Chatroom(db.Model):
+    __tablename__ = 'chatroom'
+    __table_args__ = {"useexisting": True}
+    chatroom_id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    # one-to-many
+    messages = db.relationship(
+        'Message',
+        lazy='subquery',
+        backref=db.backref('chatroom', lazy='joined')
+    )
 
 
-class Book(db.Model):
-    __tablename__ = 'book'
-    book_id = db.Column(db.Integer, primary_key=True)
-    own_id = db.Colum(db.Integer, db.ForeignKey('user.id'))
+class Message(db.Model):
+    __tablename__ = 'message'
+    __table_args__ = {"useexisting": True}
+    message_id = db.Column(db.Integer, primary_key=True)
+    message_type = db.Column(
+        ENUM('video', 'img', 'link', 'text'),
+        nullable=False
+    )
+    chatroom_id = db.Column(db.Integer, db.ForeignKey('chatroom.chatroom_id'))
